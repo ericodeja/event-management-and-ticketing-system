ff59f49c2104ddc10a0c55de691868eb84391dee
Eric Fidelis <ericodejafidelis@gmail.com>
2026-02-11 14:24:34 +0100

    Update: added auth middleware to routes

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,18 +1,19 @@
 import express from "express";
 import eventControllers from "../controllers/eventController.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/create-event", eventControllers.createEvent);
+router.post("/create-event", auth, eventControllers.createEvent);
 
-router.get("/all-events", eventControllers.getEvent);
+router.get("/all-events", auth, eventControllers.getEvent);
 
-router.get("/:id", eventControllers.getEventById);
+router.get("/:id", auth, eventControllers.getEventById);
 
-router.put("/:id", eventControllers.updateEvent);
+router.put("/:id", auth, eventControllers.updateEvent);
 
-router.put("/:id/:action", eventControllers.publishEvent);
+router.put("/:id/:action", auth, eventControllers.publishEvent);
 
-router.delete("/:id", eventControllers.deleteEvent);
+router.delete("/:id", auth, eventControllers.deleteEvent);
 
 export default router;
